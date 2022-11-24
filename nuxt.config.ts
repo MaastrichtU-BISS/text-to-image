a8ba3a966ee174f5d0f9dcab49b2f7b36826399c
CvdL-UM <c.vanderlans@maastrichtuniversity.nl>
2022-11-24 15:28:08 +0100

    Added title and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,18 @@
 export default defineNuxtConfig({
   css: ['~/assets/styles/main.scss'],
 
+  app: {
+    head: {
+      title: 'Text to image',
+      meta: [
+        { name: 'description', content: 'Input text and receive an image in return!' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/svg', href: '/BISS-favicon.svg' }
+      ]
+    }
+  },
+
   runtimeConfig: {
     apiKey: '',
   },
